7cfad0975230063dabe7f63c886d65a44d0e5c32
Sean McKinney <seanamckinney8@gmail.com>
2022-06-13 11:50:33 -0500

    Add project files.

---
+++ HFCSPg409CardExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HFCSPg409CardExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
